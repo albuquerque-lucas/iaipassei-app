2801e330ebbfd1fa435459b4593fc5eaced6c80c
Lucas Albuquerque <Lucas Albuquerque>
2024-06-25 20:43:33 -0300

    Instalando Laravel Socialite e iniciando controlador para autenticacao via Google

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
