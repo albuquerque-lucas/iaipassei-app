05a1059677b29345f085120bc50db6c0e08e3bcb
Lucas Albuquerque <Lucas Albuquerque>
2024-07-03 18:04:05 -0300

    Adicionando lista de questoes a view edit de exam

--- resources/views/admin/exams/edit.blade.php
+++ resources/views/admin/exams/edit.blade.php
@@ -13,6 +13,26 @@
     <div class="tab-content" id="editExamTabContent">
         <div class="tab-pane fade show active" id="show" role="tabpanel" aria-labelledby="show-tab">
             <x-cards.exam-info-card :exam="$exam" />
+
+            <h4 class="mt-5">Questões</h4>
+            @foreach ($examQuestions as $question)
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Questão {{ $question->question_number }}</h5>
+                        <p class="card-text">{{ $question->statement ?? 'Enunciado não informado' }}</p>
+                        <ul class="list-group" style="cursor:pointer">
+                            @foreach ($question->alternatives as $alternative)
+                                <li class="list-group-item list-group-item-action">{{ $alternative->letter }}. {{ $alternative->text }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+
+
+            <div class="d-flex justify-content-center">
+                {{ $examQuestions->links('pagination::bootstrap-4') }}
+            </div>
         </div>
         <div class="tab-pane fade" id="edit" role="tabpanel" aria-labelledby="edit-tab">
             <div class="mt-4">
