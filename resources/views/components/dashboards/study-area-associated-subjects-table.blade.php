3ce28c5dde537f1d43eb256a462b5edb34053fd7
Lucas Albuquerque <Lucas Albuquerque>
2024-07-13 15:19:16 -0300

    Corrigindo background dos botoes de study-area-associated-subjects-table

--- resources/views/components/dashboards/study-area-associated-subjects-table.blade.php
+++ resources/views/components/dashboards/study-area-associated-subjects-table.blade.php
@@ -40,7 +40,7 @@
                     <td>{{ $subject->id }}</td>
                     <td>{{ $subject->title }}</td>
                     <td>
-                        <button type="button" class="btn btn-sm bg-dark text-white" data-bs-toggle="modal" data-bs-target="#confirmDeleteSubjectModal{{ $subject->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Excluir Matéria">
+                        <button type="button" class="btn btn-sm btn-dark delete-btn" data-bs-toggle="modal" data-bs-target="#confirmDeleteSubjectModal{{ $subject->id }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Excluir Matéria">
                             <i class="fas fa-trash"></i>
                         </button>
                     </td>
