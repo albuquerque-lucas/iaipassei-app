d66c5572e671b7d7520d2ef53f0565dcd6d55b05
Lucas Albuquerque <Lucas Albuquerque>
2024-08-31 14:25:45 -0300

    Criando CalculateRankingJob

---
+++ app/Jobs/CalculateRankingJob.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Exam;
+use App\ExamStatisticsTrait;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Exception;
+
+class CalculateRankingJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, ExamStatisticsTrait;
+
+    protected $exam;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Exam $exam)
+    {
+        $this->exam = $exam;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        try {
+            $rankings = $this->calculateUserRankings($this->exam->id);
+
+            foreach ($rankings as $position => $ranking) {
+                $this->exam->rankings()->updateOrCreate(
+                    ['user_id' => $ranking['user']->id],
+                    ['position' => $position + 1, 'score' => $ranking['correct_answers']]
+                );
+            }
+
+        } catch (Exception $e) {
+            Log::error('Erro ao calcular o ranking: ' . $e->getMessage());
+        }
+    }
+}
