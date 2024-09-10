1a96eaad1788127ac530a694642a58376052e9ad
Lucas Albuquerque <Lucas Albuquerque>
2024-09-10 10:16:35 -0300

    alterando userexamseeder

--- database/seeders/UserExamSeeder.php
+++ database/seeders/UserExamSeeder.php
@@ -9,24 +9,17 @@ use App\Models\Exam;
 
 class UserExamSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // Buscar todos os usuários exceto o que tem username 'albuquerque.lucas'
         $users = User::where('username', '!=', 'albuquerque.lucas')->get();
 
         foreach ($users as $user) {
-            // Buscar o último Examination associado ao usuário
             $lastExamination = $user->examinations()->latest('id')->first();
 
             if ($lastExamination) {
-                // Buscar o último Exam associado ao Examination
                 $lastExam = $lastExamination->exams()->latest('id')->first();
 
                 if ($lastExam) {
-                    // Associar o usuário ao último Exam
                     $user->exams()->attach($lastExam->id);
                 }
             }
